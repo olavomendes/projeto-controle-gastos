84f43bc9c1d5718ce9485dc3f787d9ce17242458
Olavo Mendes <olavo.77@hotmail.com>
2020-10-30 22:27:34 -0300

    Adicionado comentários para melhor interpretação do código

--- TelaCadastro.cs
+++ TelaCadastro.cs
@@ -59,27 +59,35 @@ namespace SOS
         {
             try {
 
+                // CRIA A CONEXÃO COM O BANCO. LEMBRE-SE DE SUBSTITUIR OS CAMPOS PELOS DADOS
+                // DO SEU BANCO LOCAL
                 conexao = new SqlConnection("Server=endereco;Database=database;Uid=usuario;Pwd=senha");
-                
+
+                // QUERO PARA INSERIR OS DADOS NO BD LOCAL
                 strSQL = "INSERT INTO usuario(nome, senha, email) VALUES (@usuario, @senha, @email)";
 
+                // COMANDO DA CONEXÃO
                 comando = new SqlCommand(strSQL, conexao);
 
+                // CAPTURA OS DADOS DIGITADOS PELO USUÁRIO
                 comando.Parameters.AddWithValue("@usuario", usuarioCadastro.Text);
                 comando.Parameters.AddWithValue("@senha", senhaCadastro.Text);
                 comando.Parameters.AddWithValue("@email", emailCadastro.Text);
 
-
+                // ABRE A CONEXÃO
                 conexao.Open();
+                // EXECUTA A QUERY
                 comando.ExecuteNonQuery(); 
                 
             }
+            // CAPTURA O ERRO, CASO AJA
             catch(Exception ex) {
                 
                 MessageBox.Show(ex.Message);
 
             }
-            
+
+            // APÓS A EXECUÇÃO, FECHA A CONEXÃO E REDEFINE OS VALORES
             finally {
 
                 conexao.Close();
